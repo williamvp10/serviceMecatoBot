e2c86ee08b35391134618873a3eb1e2e922ef273
fabian <fabian.giraldo@gmail.com>
2017-04-18 00:50:50 -0500

    Ready to deploy

--- src/main/java/MainGlassfish.java
+++ src/main/java/MainGlassfish.java
@@ -1,6 +1,5 @@
+import edu.co.sergio.mundo.dao.CreateDataBase;
 import java.io.File;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.glassfish.embeddable.CommandResult;
 import org.glassfish.embeddable.CommandRunner;
 import org.glassfish.embeddable.Deployer;
@@ -12,63 +11,30 @@ import org.glassfish.embeddable.archive.ScatteredArchive;
 public class MainGlassfish {
 
 	public static void main(String[] args) throws Exception {
-		String webappDirLocation = "src/main/webapp/";
+	   String webappDirLocation = "src/main/webapp/";
 
-		// The port that we should run on can be set into an environment
-		// variable
-		// Look for that variable and default to 8080 if it isn't there.
-		String webPort = System.getenv("PORT");
-		if (webPort == null || webPort.isEmpty()) {
-			webPort = "8090";
-		}
+	// Look for that variable and default to 8080 if it isn't there.
+	String webPort = System.getenv("PORT");
+	if (webPort == null || webPort.isEmpty()) {
+	    webPort = "8090";
+	}
 
-		/** Create and start GlassFish which listens at 8080 http port */
-		GlassFishProperties gfProps = new GlassFishProperties();
-		gfProps.setPort("http-listener", Integer.valueOf(webPort)); // refer
-                gfProps.setProperty("domain-dir", "glassfishDomain");
+	/** Create and start GlassFish which listens at 8080 http port */
+	GlassFishProperties gfProps = new GlassFishProperties();
+        gfProps.setPort("http-listener", Integer.valueOf(webPort)); // refer
+        gfProps.setProperty("domain-dir", "glassfishDomain");
 
-		GlassFish glassfish = GlassFishRuntime.bootstrap()
-				.newGlassFish(gfProps);
-		glassfish.start();
-		
-	    CommandRunner runner = glassfish.getCommandRunner();
-//	    
-        String dbUrl = System.getenv("DATABASE_URL");
-        
-        
-        
-        
-        System.out.println("-------db url: " + dbUrl);
-        Matcher matcher = Pattern.compile("postgres://(.*):(.*)@(.*)/(.*)").matcher(dbUrl);
-        matcher.find();
-        
-        String host = matcher.group(3);
-        String database = matcher.group(4);
-        String user = matcher.group(1);
-        String password = matcher.group(2);
-        
-        String properties = "user=craxunyjyzohnu" + ":password=c87190c6c75542cced54dc81cf7b8c36532fe5fdb6121a599a9349942a84502a" + ":databasename=d83c0vsmvbu2e8" + ":loglevel=4:servername=ec2-23-21-204-166.compute-1.amazonaws.com" + " ";
-        System.out.println("-------properties: " + properties);
-        
-        CommandResult result = runner.run("create-jdbc-connection-pool", "--datasourceclassname", "org.postgresql.ds.PGSimpleDataSource", "--restype", "javax.sql.DataSource", 
-       // 		"--property", "url='" + dbUrl + "'", 
-       // 		"--property", "user=user:password=postgres:databasename=petclinic:server=localhost:port=5432",
-       // 		"--steadypoolsize", "1",
-       // 		"--maxpoolsize", "1",
-        		"--property", properties,
-        		"app/jdbc/petcatalog_pool");
-        
-        System.out.println("------output of create conn pool: " + result.getOutput());
-        
-         result = runner.run("create-jdbc-resource", "--connectionpoolid", "app/jdbc/petcatalog_pool", 
-	    		"app/jdbc/petcatalog");
-	    
-        System.out.println("------output of create jdbc: " + result.getOutput());
-
-        result  = runner.run("set-log-level", "javax.enterprise.system.container.web=INFO:javax.enterprise.system.container.ejb=FINEST");
+	GlassFish glassfish = GlassFishRuntime.bootstrap()
+			.newGlassFish(gfProps);
+	glassfish.start();
+	CommandRunner runner = glassfish.getCommandRunner();
+	
+        CommandResult result  = runner.run("set-log-level", "javax.enterprise.system.container.web=INFO:javax.enterprise.system.container.ejb=FINEST");
         System.out.println("------output of set log level: " + result.getOutput());
 	Deployer deployer = glassfish.getDeployer();
 
+        //Invocando el Script de Creacion de la base de datos.
+        CreateDataBase.run();
 	// Create a scattered web application.
 	ScatteredArchive archive = new ScatteredArchive("myApp",
 	ScatteredArchive.Type.WAR, new File(webappDirLocation));
