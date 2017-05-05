d1ead579ef1037ce365ca2406989d0c8420689ee
fabian <fabian.giraldo@gmail.com>
2017-05-04 22:51:33 -0500

    MVC - JFreechart

---
+++ src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crunchify.jsp.servlet;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+public class ChartServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        	response.setContentType("image/png");
+		OutputStream outputStream = response.getOutputStream();
+		JFreeChart chart = getChart();
+		int width = 500;
+		int height = 350;
+		ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
+
+	}
+
+	public JFreeChart getChart() {
+		DefaultPieDataset dataset = new DefaultPieDataset();
+		dataset.setValue("Ford", 23.3);
+		dataset.setValue("Chevy", 32.4);
+		dataset.setValue("Yugo", 44.2);
+
+		boolean legend = true;
+		boolean tooltips = false;
+		boolean urls = false;
+
+		JFreeChart chart = ChartFactory.createPieChart("Cars", dataset, legend, tooltips, urls);
+
+		chart.setBorderPaint(Color.GREEN);
+		chart.setBorderStroke(new BasicStroke(5.0f));
+		chart.setBorderVisible(true);
+
+		return chart;
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -37,5 +37,6 @@ body {
       
     
     %>
+    <img alt="grafico" src=Porcentaje.jpeg"/>
 </body>
 </html>
