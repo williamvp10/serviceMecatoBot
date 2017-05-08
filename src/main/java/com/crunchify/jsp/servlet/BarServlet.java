c9a4109f726729a8d93371aa181b634b043db8f7
Juliana Diaz <juli.milena@hotmail.com>
2017-05-08 10:27:42 -0500

    Despliegue Inicial - barras

---
+++ src/main/java/com/crunchify/jsp/servlet/BarServlet.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crunchify.jsp.servlet;
+
+
+import java.awt.Color;
+import java.awt.Paint;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.labels.ItemLabelAnchor;
+import org.jfree.chart.labels.ItemLabelPosition;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.general.DatasetUtilities;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+import org.jfree.ui.TextAnchor;
+
+public class BarServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        	response.setContentType("image/png");
+		OutputStream outputStream = response.getOutputStream();
+		JFreeChart chart = getChart();
+		int width = 500;
+		int height = 350;
+		ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
+
+	}
+
+	public JFreeChart getChart() {
+		
+        
+         double[][] data = new double[][] {{4.0, 3.0, -2.0, 3.0, 6.0}};
+         CategoryDataset category = DatasetUtilities.createCategoryDataset(
+            "Series ",
+            "Category ",
+            data
+        );
+        
+ JFreeChart chart = ChartFactory.createBarChart(
+            "Bar Chart Demo 3",       // chart title
+            "Category",               // domain axis label
+            "Value",                  // range axis label
+            category,                  // data
+            PlotOrientation.VERTICAL, // the plot orientation
+            false,                    // include legend
+            true,
+            false
+        );
+
+        chart.setBackgroundPaint(Color.lightGray);
+
+        // get a reference to the plot for further customisation...
+        CategoryPlot plot = chart.getCategoryPlot();
+        plot.setNoDataMessage("NO DATA!");
+
+        CategoryItemRenderer renderer = new CustomRenderer(
+            new Paint[] {Color.red, Color.blue, Color.green,
+                Color.yellow, Color.orange, Color.cyan,
+                Color.magenta, Color.blue}
+        );
+
+        renderer.setItemLabelsVisible(true);
+        ItemLabelPosition p = new ItemLabelPosition(
+            ItemLabelAnchor.CENTER, TextAnchor.CENTER, TextAnchor.CENTER, 45.0
+        );
+        renderer.setPositiveItemLabelPosition(p);
+        plot.setRenderer(renderer);
+
+        // change the margin at the top of the range axis...
+        ValueAxis rangeAxis = plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        rangeAxis.setLowerMargin(0.15);
+        rangeAxis.setUpperMargin(0.15);
+
+        return chart;
+		
+	}
+
+}
---
+++ src/main/java/com/crunchify/jsp/servlet/CustomRenderer.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crunchify.jsp.servlet;
+
+import java.awt.Paint;
+import org.jfree.chart.renderer.category.BarRenderer;
+
+/**
+ *
+ * @author Labing
+ */
+ class CustomRenderer extends BarRenderer {
+
+        /** The colors. */
+        private Paint[] colors;
+
+        /**
+         * Creates a new renderer.
+         *
+         * @param colors  the colors.
+         */
+        public CustomRenderer(final Paint[] colors) {
+            this.colors = colors;
+        }
+
+        /**
+         * Returns the paint for an item.  Overrides the default behaviour inherited from
+         * AbstractSeriesRenderer.
+         *
+         * @param row  the series.
+         * @param column  the category.
+         *
+         * @return The item color.
+         */
+        public Paint getItemPaint(final int row, final int column) {
+            return this.colors[column % this.colors.length];
+        }
+    }
