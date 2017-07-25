e36130c0b1e68e186b38b381b02be04be7cc644f
fabian.giraldo <fabian.giraldo@fgiraldo.usa.loc>
2017-07-25 15:51:26 -0500

    Ejercicio CRUD Jersey

--- src/main/java/com/mkyong/rest/HelloWorldService.java
+++ src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -18,7 +18,6 @@ public class HelloWorldService {
 	public Response getMsg(@PathParam("param") String msg) {
 
 		String output = "Jersey say : " + msg;
-
 		return Response.status(200).entity(output).build();
 
 	}
