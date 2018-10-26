7b56da60db1a06858d7b7c7dfaa617a3d380aadc
Carlos Alberto <Carlos Alberto@DESKTOP-8DSF92O>
2018-10-26 12:13:32 -0500

    Dao y Vo venec

---
+++ src/main/java/VO/Producto.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VO;
+
+/**
+ *
+ * @author Carlos Alberto
+ */
+public class Producto {
+    
+    String id;
+    String nombre;
+    String tipo;
+    Tienda idTienda;
+    String ingredientes;
+    int precio;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Tienda getIdTienda() {
+        return idTienda;
+    }
+
+    public void setIdTienda(Tienda idTienda) {
+        this.idTienda = idTienda;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
+    
+    
+}
